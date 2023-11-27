0ef3202cfc09a4f4f3b72eaa754fbfb2b3e1b39f
unknown <shahzadanouman@hotmail.com>
2023-11-27 20:26:25 +0500

    media queries, js data types and operators 27-11-23

---
+++ JS/day 13/javascript.js
@@ -0,0 +1,114 @@
+
+// methods to display output in browser
+
+// 1 method
+// alert('hello world! External file')
+
+// 2nd method
+// console.log("Hello World!");
+
+// 3rd method
+document.write('hello world!')
+document.write(' <h1> hello world! </h1>')
+
+
+
+// data types
+
+// integer
+
+let a = 10
+// let b = 0
+console.log(a);
+
+// decimal
+let d = 10.25
+console.log(d);
+
+// string
+let s = 'hello world!'
+let s1 = ' 3487345371 hello world! 63178168.34564 (*&(*&(*^(*^^%#$@@#'
+
+console.log(s)
+console.log(s1)
+
+// boolean bool
+
+var b = true
+var b1 = false
+
+console.log(b)
+console.log(b1)
+
+var b = 0
+console.log(b)
+
+// array 
+
+let e = [12,12,455,7887]
+console.log(e)
+console.log(e[0])
+console.log(e[1])
+console.log(e[2])
+console.log(e[3])
+console.log(e[4])
+
+let mix= [25,41.0,'hello world',true,[12,36] ]
+console.log(mix)
+console.log(mix[4][1])
+
+// two diment`ional array
+let two_d_arr = [ [1,2,3,4,5,6] ]
+console.log(two_d_arr)
+
+console.log(two_d_arr[1])
+
+
+// three dimentional array
+
+let three_d_arr = [ [ [1,2,3,4,5,6] ]]
+
+console.log(three_d_arr)
+console.log(three_d_arr[0][0][1])
+
+// assignment operator (=)
+let z = 25
+
+// artithmetic operators (+,-,*,/,%,**,++,--)
+let y = 15
+
+// +
+console.log( z+y )
+console.log( z+ 100 )
+console.log( 35 + 100 )
+
+// - 
+console.log( z - y)
+
+// *
+console.log( z * y)
+
+// /
+console.log( z / y)
+
+// %
+console.log( z % y)
+console.log( z % 5)
+
+// **
+
+console.log(2**2)
+console.log(2**3)
+
+// ++
+
+// z = z+1
+z++
+console.log(z)
+
+// --
+
+// y = y-1
+y--
+
+console.log( y)
