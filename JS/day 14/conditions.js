5c60cf54dde93ecc25e17ebe60bd91b399c09b6b
unknown <shahzadanouman@hotmail.com>
2023-11-28 20:20:45 +0500

    conditions and conditionals operators - 28-11-23

---
+++ JS/day 14/conditions.js
@@ -0,0 +1,183 @@
+// alert('hello world!')
+// console.log('hello world!')
+// console.log("hello world!")
+// document.write('hello world!')
+
+
+// artithmetic operators 
+// + - * / % ++ -- **
+
+// conditional operators
+// > < >= <= == === != !==
+
+let page = 'navbar'
+
+// if conditions
+// let page = 'shop'
+
+// if( page == 'home' ){
+//     console.log('Home page')
+// }
+
+// if( page == 'shop' ){`
+//     console.log('Shop page')
+// }
+
+// if else
+
+// if( page == 'home' ){
+//     console.log('Home page')
+// } else {
+//     console.log('Not Home page')
+// }
+
+
+// if else if
+
+// if( page == 'home' ){
+//     console.log('Home page')
+// } else if(page == 'shop') {
+//     console.log('Shop page')
+// } else if(page =='navbar'){
+//     console.log('Nav bar page')
+// } else {
+//     console.log('condition false')
+// }
+
+
+let a = '15'
+let b =10
+
+console.log(a)
+console.log(b)
+// > greate than
+// if( a>b ){
+//     console.log(' A is greater')
+// } else{
+//     console.log(' B is greater')
+// }
+
+// < less than
+// if( a<b ){
+//     console.log(' A is Smaller')
+// } else{
+//     console.log(' B is Smaller')
+// }
+
+// <= less than and equall to
+
+// if( a<=b ){
+//     console.log(' A is Smaller or equall')
+// } else{
+//     console.log(' B is Smaller')
+// }
+
+// >= greater than and equall to
+// if( a>=b ){
+//     console.log(' A is greater  or equall')
+// } else{
+//     console.log(' B is greater')
+// }
+
+// == equall to
+
+// if( a == b ){
+//     cog('onsole.l values are equall')
+// } else{
+//     console.log(' values are not equall')
+// }
+
+// === check values and data type also
+
+// if( a === b ){
+//     console.log(' values and datatype are equall')
+// } else{
+//     console.log(' values or datatype are not equall')
+// }
+
+
+// ==  != values
+// === !== values and data types
+// != return true if values are not equall
+
+// if( a != b ){
+//     console.log(' values are not equall')
+// } else{
+//     console.log(' values are equall')
+// }
+
+// !== return true if values or data type are not equall
+
+// if( a !== b ){
+//     console.log(' values or datatype are not equall')
+// } else{
+//     console.log(' values are equall')
+// }
+
+// logical operators
+// AND && , or || , NOT !
+
+// AND will return true if all conditions are true
+
+let age =75 
+console.log(age)
+// if( age>=18 && age<=65){
+//     console.log('Allowed')
+// }else{
+//     console.log('Not allowed')
+// }
+
+// OR will return true if any condition is true
+
+// if( age>=18 || age<=65){
+//     console.log('Allowed')
+// }else{
+//     console.log('Not allowed')
+// }
+
+// Not ! reverse the condition
+// if(!a>=b ){
+//     console.log(' A is greater  or equall')
+// } else{
+//     console.log(' B is greater')
+// }
+
+
+// Grading example
+
+let marks=89.99
+console.log('Percentange is: ' + marks)
+
+// if(marks>=90){
+//     console.log('A Grade')
+// }else if(marks >= 80){
+//     console.log('B Grade')
+// }else if(marks >= 70){
+//     console.log('C Grade')
+// }else if(marks >= 60){
+//     console.log('D Grade')
+// }else if(marks >=50){
+//     console.log('E Grade')
+// } else {
+//     console.log('Fail')
+// }
+
+if(marks>=50 && marks<60){
+    console.log('E Grade')
+}else if(marks >= 60 && marks<70 ){
+    console.log('D Grade')
+}else if(marks >= 70 && marks<80){
+    console.log('C Grade')
+}else if(marks >= 80 && marks<90){
+    console.log('B Grade')
+}else if(marks >=90){
+    console.log('A Grade')
+} else {
+    console.log('Fail')
+}
+
+
+console.log(10%2)
+
+
+
