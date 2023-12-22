312fef5162ada1ec85500247dd5d4dd4eab99a72
unknown <shahzadanouman@hotmail.com>
2023-12-22 20:09:29 +0500

    python var and data types 22-12-23

---
+++ python/data_types.py
@@ -0,0 +1,133 @@
+# To display something 
+print('Hello World!')
+
+''' 
+print('Hello World!')
+print('Hello World!')
+print('Hello World!')
+print('Hello World!')
+
+'''
+""" 
+print('Hello World!')
+print('Hello World!')
+print('Hello World!')
+print('Hello World!')
+"""
+
+# declare new variable
+
+a = 0 
+print(a)
+
+a = 10
+print(a)
+
+
+# data types
+number = 20
+decimal = 20.36131
+string = "Hello world!"
+boolian = False
+array = [1,2,5,4,85,87,4]
+
+print(number)
+print(type(number) )
+
+print(decimal)
+print( type(decimal))
+
+print(string)
+print(type(string))
+
+print(boolian)
+print(type(boolian))
+
+print(array)
+print(array[6])
+print(type(array))
+
+
+# string formatting
+
+print('formating :' + string)
+
+# format function 
+# print( "Number:{}  ---  Decimal:{} ".format(number,decimal) )
+
+# print("Number:{}  ---  Decimal:{}  --- Bool :{}  --- String: {}  --- List: {} ".format(number,decimal,boolian,string,array[5]) )
+
+# f-string formating
+
+# print( f"Number:{number}  ---  Decimal:{decimal} " )
+# print( f'Number :{number} --- Decimal: {decimal} --- string: {string} --- boolian: {boolian} -- list: {array} ' )
+
+# formated_string = f"Number:{number}  ---  Decimal:{decimal} "
+
+# print(formated_string  )
+
+
+# percentage formating
+print( 'number: %s ' %(number)  )
+
+
+# arthimetic operators
+# + - * ** / % //
+
+#  +
+
+a = 10
+b = 25
+
+print(a + b)
+
+
+# - 
+
+print(a - b )
+print(b - a )
+
+# mul *
+
+print(a*b)
+
+# /
+
+print(a/b)
+print(b/a)
+
+# %
+
+print(a%2)
+print(a%3)
+print(a%4)
+
+# power **
+
+print(a**b)
+print(a**2)
+
+# //
+
+print(a//b)
+print(b//a)
+
+y = 10
+
+z = 0
+
+# z = z + 1
+z += y
+
+print(f'z: {z}')
+
+TEST= 10
+print(TEST)
+
+TEST = 'string'
+print(TEST)
+
+TEST = True
+print(TEST)
+
+
