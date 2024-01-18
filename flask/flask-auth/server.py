bbaab35eb84825a6324e12706cc65a6bec45aede
unknown <shahzadanouman@hotmail.com>
2024-01-18 21:01:05 +0500

    quiz questions- add and display - 18-01-24

--- flask/flask-auth/server.py
+++ flask/flask-auth/server.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, flash, redirect, url_for, session
 from mysql import connector
+import json
 
 app = Flask(__name__)
 app.secret_key = 'lskjdfslkfjlksjflksjflslfkjsd'
@@ -15,8 +16,25 @@ def index():
 def quiz():
     if 'user_name' in session:
         user_name = session.get('user_name',None)
+        db = connection.cursor()
+        db.execute("SELECT title, options, answer FROM questions")
+        all_questions = db.fetchall()
+        
+        quiz_questions = []
 
-        return render_template('quiz.html',user_name=user_name)
+        for question in all_questions:
+            current_question = {
+                'title' : question[0],
+                'options' : json.loads(question[1]),
+                'answer' : question[2]
+            }
+            quiz_questions.append(current_question)
+
+
+        # quiz_questions = json.dumps(quiz_questions)
+        # return quiz_questions
+        # return all_questions
+        return render_template('quiz.html',user_name=user_name,questions=quiz_questions)
     else:
         flash('kindly login first!','danger')
         return redirect(url_for('login'))
@@ -84,9 +102,6 @@ def signup():
             db.close()
             flash('User registered successfully!','success')
             return redirect(url_for('index'))
-        
-            
-
 
 @app.route('/login',methods=['GET','POST'])
 def login():
@@ -127,6 +142,73 @@ def logout():
 
     flash('user logged out Successfully!','success')
     return redirect(url_for('login'))
+
+@app.route('/test',methods=['GET','POST'])
+def test():
+    if request.method == 'GET':
+        return render_template('test.html')
+    else:
+        first_name = request.form.getlist('first_name')
+        return first_name
+        # return request.form
+
+@app.route('/questions')
+def questions():
+    db = connection.cursor()
+    db.execute("SELECT * FROM questions")
+    all_questions =  db.fetchall()
+    db.close()
+
+    # return all_questions
+    # options = json.loads(all_questions[3][2])
+    # return options[0]
+
+    return render_template('questions.html',questions = all_questions)
+
+@app.route('/add-question',methods=['GET','POST'])
+def add_question():
+    if request.method == 'GET':
+        return render_template('add_question.html')
+    else:
+        errors = False
+        options_error = False
+        title = request.form.get('title')
+        options = request.form.getlist('options')
+        answer = request.form.get('answer')
+
+
+        if not title:
+            flash('Title is required','danger')
+            errors = True
+
+        for option in options:
+            if not option:
+                if not options_error:
+                    options_error = True
+                    errors = True
+                    flash('All options are required','danger')
+                    break
+
+        if not answer:
+            flash('Correct answer is required','danger')
+            errors = True
+
+        if errors:
+            return redirect( url_for('add_question'))
+        else:
+            # type casting
+            options = json.dumps(options)
+
+            # return options
+            db = connection.cursor()
+            db.execute("INSERT INTO questions (title,options,answer) VALUES(%s,%s,%s)",(title,options,answer))
+            connection.commit()
+            db.close()
+
+            flash('New Question Added successfully!','success')
+            return redirect(url_for('questions'))
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
